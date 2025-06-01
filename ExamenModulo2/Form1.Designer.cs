33ed69e2d8c544c914f460b28d54db509cef819a
Jose <urdaneta.j@outlook.com>
2025-06-01 17:37:43 -0500

    Add project files.

---
+++ ExamenModulo2/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ExamenModulo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel2 = new Panel();
+            label3 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Location = new Point(12, 107);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(798, 421);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label1.Location = new Point(27, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Player X";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label2.Location = new Point(24, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Player O";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 28);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 85);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(148, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(108, 85);
+            button2.TabIndex = 3;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(274, 27);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 85);
+            button3.TabIndex = 4;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(12, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(798, 89);
+            panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 22F, FontStyle.Bold);
+            label3.Location = new Point(41, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(728, 50);
+            label3.TabIndex = 1;
+            label3.Text = "JUEGO TIC TAC TOC - WINDOWS FORMS";
+            label3.Click += label3_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button7);
+            panel3.Controls.Add(button8);
+            panel3.Controls.Add(button9);
+            panel3.Controls.Add(button4);
+            panel3.Controls.Add(button5);
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(button3);
+            panel3.Location = new Point(26, 33);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(416, 346);
+            panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(textBox1);
+            panel4.Controls.Add(label1);
+            panel4.Controls.Add(label2);
+            panel4.Location = new Point(475, 33);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(294, 162);
+            panel4.TabIndex = 6;
+            panel4.Paint += panel4_Paint;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button12);
+            panel5.Controls.Add(button11);
+            panel5.Controls.Add(button10);
+            panel5.Location = new Point(475, 221);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(294, 158);
+            panel5.TabIndex = 7;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(148, 130);
+            button4.Name = "button4";
+            button4.Size = new Size(108, 85);
+            button4.TabIndex = 6;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(24, 130);
+            button5.Name = "button5";
+            button5.Size = new Size(108, 85);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(274, 130);
+            button6.Name = "button6";
+            button6.Size = new Size(108, 85);
+            button6.TabIndex = 7;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(148, 234);
+            button7.Name = "button7";
+            button7.Size = new Size(108, 85);
+            button7.TabIndex = 9;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(24, 234);
+            button8.Name = "button8";
+            button8.Size = new Size(108, 85);
+            button8.TabIndex = 8;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(274, 234);
+            button9.Name = "button9";
+            button9.Size = new Size(108, 85);
+            button9.TabIndex = 10;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(13, 18);
+            button10.Name = "button10";
+            button10.Size = new Size(266, 59);
+            button10.TabIndex = 0;
+            button10.Text = "New Game";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(13, 83);
+            button11.Name = "button11";
+            button11.Size = new Size(129, 59);
+            button11.TabIndex = 1;
+            button11.Text = "Reset";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(150, 83);
+            button12.Name = "button12";
+            button12.Size = new Size(129, 59);
+            button12.TabIndex = 2;
+            button12.Text = "Exit";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(150, 96);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(119, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(150, 36);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(119, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DodgerBlue;
+            ClientSize = new Size(831, 555);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private Label label3;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel5;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ ExamenModulo2/Form1.cs
@@ -0,0 +1,20 @@
+namespace ExamenModulo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
