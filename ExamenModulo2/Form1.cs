8dcd136f576e0fc1858cf5d8aa06ad96af350f48
Jose <urdaneta.j@outlook.com>
2025-06-01 18:01:26 -0500

    Primer Avance

--- ExamenModulo2/Form1.cs
+++ ExamenModulo2/Form1.cs
@@ -2,6 +2,26 @@ namespace ExamenModulo2
 {
     public partial class Form1 : Form
     {
+        Boolean checker;
+        int plusOne;
+
+        public void Enable_False()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        public void Score()
+        {
+           
+        }
         public Form1()
         {
             InitializeComponent();
